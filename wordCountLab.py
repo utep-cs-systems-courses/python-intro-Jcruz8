143b75579ba15b5ec0a5dcbbc66542b1e477bfe5
Johnny <jcruz8@miners.utep.edu>
2020-09-01 20:34:29 -0600

    only writes once

--- wordCountLab.py
+++ wordCountLab.py
@@ -1,19 +1,18 @@
 f = open("testfile.txt", "r")
 o =open("output.txt", "w").close()
 
-words = {}
+wordsDict = {}
 
 #print(f.read())
 for line in f:
     for word in line.split():
-        if word in words:
-            words[word] += 1
+        if word in wordsDict:
+            wordsDict[word] += 1
         else:
-            words[word] = 1
-    o = open("output.txt", "a")
-    o.write(str(words))
-    o.close()
-            
-        #print(word)
-        
+            wordsDict[word] = 1
+#sortedWordsDict = {}
+#sortedWordsDict = sorted(wordsDict, reverse = True)
+o = open("output.txt", "a")
+o.write(str(wordsDict))
+o.close()
 f.close()
