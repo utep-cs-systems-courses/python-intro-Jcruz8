dd915aacab93e578c1e233cdce334ea9a8313c9b
Johnny <jcruz8@miners.utep.edu>
2020-09-02 12:15:40 -0600

    done

--- wordCountLab.py
+++ wordCountLab.py
@@ -1,12 +1,9 @@
 import sys
 f = sys.argv[1]
 o = sys.argv[2]
-#with open(f, "r") as f:
-#o =open("output.txt", "w").close()
-
 wordsDict = dict()
 
-#print(f.read())
+#opens file to read
 with open(f,"r") as f:
     for line in f:
         line = line.lower()
@@ -15,16 +12,11 @@ with open(f,"r") as f:
                 wordsDict[word] += 1
             else:
                 wordsDict[word] = 1
-#sortedWordsDict = {}
-#sortedWordsDict = sorted(wordsDict, reverse = True)
-#open(o, "w")
-#o.write(str(wordsDict))
-#o.close()
-#for key in sorted(wordsDict.items(), reverse = True):
-#    o.write(key + " " + str(wordsDict[key]) + "\n")
+f.close()
 sortedDict = sorted(wordsDict.items(), key = lambda x: x[1], reverse = True)
+
+#opens file to write
 with open(o, "w") as o:
     for i in sortedDict:
         o.write(i[0] + " " + str(i[1]) + "\n")
 o.close()
-f.close()
