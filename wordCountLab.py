dd1066b1c54876a00df5f7cd27e44d2c971f505a
Johnny <jcruz8@miners.utep.edu>
2020-09-02 11:33:33 -0600

    working so far got to change some things

--- wordCountLab.py
+++ wordCountLab.py
@@ -1,10 +1,11 @@
 f = open("testfile.txt", "r")
-o =open("output.txt", "w").close()
+#o =open("output.txt", "w").close()
 
-wordsDict = {}
+wordsDict = dict()
 
 #print(f.read())
 for line in f:
+    line = line.lower()
     for word in line.split():
         if word in wordsDict:
             wordsDict[word] += 1
@@ -12,7 +13,13 @@ for line in f:
             wordsDict[word] = 1
 #sortedWordsDict = {}
 #sortedWordsDict = sorted(wordsDict, reverse = True)
-o = open("output.txt", "a")
-o.write(str(wordsDict))
+o = open("output.txt", "w")
+#o.write(str(wordsDict))
+#o.close()
+#for key in sorted(wordsDict.items(), reverse = True):
+#    o.write(key + " " + str(wordsDict[key]) + "\n")
+sortedDict = sorted(wordsDict.items(), key = lambda x: x[1], reverse = True)
+for i in sortedDict:
+    o.write(i[0] + " " + str(i[1]) + "\n")
 o.close()
 f.close()
